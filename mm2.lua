41486d4c9c60f08d924fe4a925da0f78dfb6eb94
mm2 <forfreeporno733@gmail.com>
2024-11-06 18:11:28 -0300

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,3 +1,4 @@
-loadstring(game:HttpGet("https://rawscripts.net/raw/Murder-Mystery-2-Overdrive-H-MM-15035"))()
-
-loadstring(game:HttpGet('https://darkscripts.space/scripts/a5bace7951.lua', true))()
+(function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/Murder-Mystery-2-Overdrive-H-MM-15035"))()
+    loadstring(game:HttpGet("https://darkscripts.space/scripts/a5bace7951.lua", true))()
+end)()
